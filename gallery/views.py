debd75bc66cdffa51e7d5e3741edc940ed20ead8
Ed Rahn <edsrahn@gmail.com>
2013-02-20 15:28:12 -0500

    task #9

--- gallery/views.py
+++ gallery/views.py
@@ -1,12 +1,15 @@
 from django.views.generic import ListView, DetailView
 from django.core.cache import cache
+from django.http import Http404, HttpResponse
 from models import Image
+import json
+
 
 class GalleryView(ListView):
     template_name = "gallery/list.html"
     model = Image
 
-    def get_queryset(self):
+    def get_query(self):
         title = self.request.GET.get('query', None)
 
         if title:
@@ -14,9 +17,38 @@ class GalleryView(ListView):
             if query is None:
                 query = Image.objects.filter(title__contains=title)
                 cache.set(title, query)
-            return query
+            return query 
         else:
-            return Image.objects.all()
+            return None
+
+
+    def get_queryset(self):
+        query = self.get_query()
+        if query is None:
+            query = Image.objects.all()
+
+        return query
+
+    def get(self, *args, **kwargs):
+        format = self.request.GET.get('format', None)
+        query = self.get_query()
+
+        if format == 'json' and query is None:
+            raise Http404
+        elif format == 'json' and query:
+            json_list = []
+            for image in query:
+                json_list.append({'title': image.title,
+                                'author': image.author.name,
+                                'timestamp': str(image.timestamp),
+                                'hash': image.hash,
+                                'ext': image.ext,
+                                'size': image.size,
+                                'url': image.url})
+                
+            return HttpResponse(json.dumps(json_list))
+        else:
+            return super(GalleryView, self).get(*args, **kwargs)
 
 class GalleryDetailView(DetailView):
     template_name = "gallery/detail.html"
