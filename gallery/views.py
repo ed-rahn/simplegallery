3d8acfe97082e4fe42b414c6632d008f31ef5ab1
Ed Rahn <edsrahn@gmail.com>
2013-02-20 14:37:06 -0500

    task #8

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.core.cache import cache
 from models import Image
 
 class GalleryView(ListView):
@@ -9,7 +10,11 @@ class GalleryView(ListView):
         title = self.request.GET.get('query', None)
 
         if title:
-            return Image.objects.filter(title__contains=title)
+            query = cache.get(title)
+            if query is None:
+                query = Image.objects.filter(title__contains=title)
+                cache.set(title, query)
+            return query
         else:
             return Image.objects.all()
 
