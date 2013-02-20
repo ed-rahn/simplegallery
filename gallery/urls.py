9e3aaf61811367b63c4aa67dad03167b0d2dde42
Ed Rahn <edsrahn@gmail.com>
2013-02-20 14:28:32 -0500

    tasks #7

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 
-from views import GalleryView, GalleryDetailView, GalleryByAuthorView, GalleryByTitleView
+from views import GalleryView, GalleryDetailView, GalleryByAuthorView
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', GalleryView.as_view(), name='gallery'),
     url(r'^(?P<pk>\d+)/$', GalleryDetailView.as_view(), name='gallery-detail'),
     url(r'^byauthor/(?P<authorname>\w+)/$', GalleryByAuthorView.as_view(), name='gallery-byauthor'),
-    url(r'^search-by-title$', GalleryByTitleView.as_view(), name='search-by-title'),
 )
--- gallery/views.py
+++ gallery/views.py
@@ -5,15 +5,20 @@ class GalleryView(ListView):
     template_name = "gallery/list.html"
     model = Image
 
+    def get_queryset(self):
+        title = self.request.GET.get('query', None)
+
+        if title:
+            return Image.objects.filter(title__contains=title)
+        else:
+            return Image.objects.all()
+
 class GalleryDetailView(DetailView):
     template_name = "gallery/detail.html"
     model = Image
 
 class GalleryByAuthorView(ListView):
-    pass
-
-class GalleryByTitleView(ListView):
     template_name = "gallery/list.html"
 
     def get_queryset(self):
-        return Image.objects.filter(title__contains=self.request.GET['title'])
+        return Image.objects.filter(author=self.kwargs['authorname'])
